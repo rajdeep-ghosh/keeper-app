9d229ea29c5cc265710c2b5ca596289f6f1fcb0e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-02-10 17:49:09 +0530

    Complete step 1

--- src/index.js
+++ src/index.js
@@ -10,3 +10,9 @@
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello World!</h1>, document.querySelector("#root"));
