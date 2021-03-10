344a3bec7b9c286bb8b007846785e18a3794009d
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-03-11 04:19:26 +0530

    Styling App using material-ui

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import Zoom from '@material-ui/core/Zoom';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 function CreateArea(props) {
     const [note, setNote] = useState({title: "", content: ""});
+
+    const [isExpanded, setExpanded] = useState(false);
 
     function handleChange(event) {
         const {name, value} = event.target;
@@ -20,12 +25,27 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function expand() {
+        setExpanded(true);
+    }
+
     return (
         <div>
             <form className="create-note">
-                <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
-                <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
-                <button onClick={handleClick}>Add</button>
+                {isExpanded && <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />}
+                <textarea 
+                    onChange={handleChange} 
+                    onClick={expand} 
+                    name="content" 
+                    value={note.content} 
+                    placeholder="Take a note..." 
+                    rows={isExpanded ? 3 : 1}
+                />
+                <Zoom in={isExpanded}>
+                    <Fab onClick={handleClick}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
