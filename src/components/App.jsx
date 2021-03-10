449f0602ad20aa42f47279d367ed0dfa4d9e3b1c
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-03-11 02:37:06 +0530

    Initial setup for challenge 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(note => (
-                <Note 
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                />
-            ))}
+            <CreateArea />
+            <Note title="Note Title" content="Note Content" />
             <Footer />
         </div>
     );
