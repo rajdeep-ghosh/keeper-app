7caee20e6e4fb54a402cb06ef7376d17e8c5321b
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-03-11 02:51:19 +0530

    Create constant that keeps track of note title and content

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+    const [note, setNote] = useState({title: "", content: ""});
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        });
+    }
+
     return (
         <div>
             <form>
-                <input type="text" name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." rows="3" />
+                <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
+                <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
                 <button>Add</button>
             </form>
         </div>
